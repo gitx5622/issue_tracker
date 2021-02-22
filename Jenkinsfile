6d64f104bea92edad00772319653b9655987466f
gitx5622 <ggitau5622@gmail.com>
2021-02-22 03:08:29 +0300

    cicd docker compose up

--- Jenkinsfile
+++ Jenkinsfile
@@ -30,7 +30,7 @@ pipeline {
 
                 stage ('Running tha Application'){
                     steps{
-                        sh "docker run gits5622/issue_tracker"
+                        sh "docker-compose up"
                     }
                 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const REDIRECT_URI = `http://localhost:3000`;
 export const CLIENT_ID = `Iv1.484927f49fafccc1`;
 export const uri = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
-export const token = '4c1fbe371aad9b319da0be7a9047f8abc1738d0a';
+export const token = '911c1f705f6086acb70243cbf011e2a258e30bc4';
 export const API_ROUTE = 'https://api.github.com';
 export const owner = 'gitx5622';
 export const query = "";
