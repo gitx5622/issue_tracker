b310b6d28eb976d8abee42f0e9a1dc25ad3cb403
gitx5622 <ggitau5622@gmail.com>
2021-02-24 15:59:53 +0300

    docker run

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const REDIRECT_URI = `http://localhost:3000`;
 export const CLIENT_ID = `Iv1.484927f49fafccc1`;
 export const uri = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
-export const token = '911c1f705f6086acb70243cbf011e2a258e30bc4';
+export const token = '35639d8012e6ec0fb8b9fa82a2f4492b46083b22';
 export const API_ROUTE = 'https://api.github.com';
 export const owner = 'gitx5622';
 export const query = "";
